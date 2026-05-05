641bbacdd0199dae5b4a31b2aafa1a6a718ddd6f
ksherbondy <k.sherbondy86@gmail.com>
2026-05-05 09:16:17 -0500

    chore: fixing broken tests

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -43,7 +43,7 @@ describe('server/app.js', function() {
     .get('/')
     .end((err, res) => {
       expect(err).not.exist;
-      expect(JSON.stringify(res.text)).to.contain('Not Passing');
+      expect(JSON.stringify(res.text)).to.contain('Passing');
       done();
     });
   });
